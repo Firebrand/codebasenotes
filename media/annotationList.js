b2347a185a3557afd6b0c4e484ef1f2b06429589
Alex <saltypickles@yahoo.com>
2024-10-22 09:16:00 -0500

    candidate 6

--- media/annotationList.js
+++ media/annotationList.js
@@ -4,9 +4,20 @@
 
     function renderAnnotations(annotations) {
         annotationList.innerHTML = '';
+        
+        // Sort annotations based on the first two characters
+        annotations.sort((a, b) => {
+            const aNum = parseInt(a.annotation.substring(0, 2)) || Infinity;
+            const bNum = parseInt(b.annotation.substring(0, 2)) || Infinity;
+            return aNum - bNum;
+        });
+
         annotations.forEach(item => {
             const li = document.createElement('li');
-            li.innerHTML = `${item.annotation}`;
+            const truncatedAnnotation = item.annotation.length > 100 
+                ? item.annotation.substring(0, 100) + '...' 
+                : item.annotation;
+            li.innerHTML = `${truncatedAnnotation}`;
             li.addEventListener('click', () => {
                 vscode.postMessage({ type: 'revealItem', path: item.path });
             });
